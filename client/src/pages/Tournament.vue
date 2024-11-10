3234d7e859f973cafa650a94685688b065cd1bcd
GitGudShu <shuukuro@outlook.fr>
2024-11-10 11:42:08 +0100

    Tournament Tree first version

--- client/src/pages/Tournament.vue
+++ client/src/pages/Tournament.vue
@@ -2,11 +2,11 @@
   <q-page>
     <div class="wrapper">
 
-      <!-- Here's an example of how you can use the cards :) -->
-      <!-- <div class="row">
-        <Card title="Card 1" state="En cours..." />
-        <Card title="Card 1" state="En cours..." />
-      </div> -->
+      <div class="text-h4 text-center text-primary text-bold">TOURNOIS</div>
+
+      <div class="tree-container">
+        <TournamentTree name="Tournoi de test" :rounds="testRounds"/>
+      </div>
 
       <div class="row">
         <template v-for="game in allGames">
@@ -22,9 +22,68 @@
 import { onMounted, ref } from 'vue';
 import { api } from 'src/boot/axios';
 import Card from 'src/components/Card.vue';
-
+import TournamentTree from 'src/components/TournamentTree.vue';
 
 const allGames = ref([]);
+const testRounds = [
+//Quarter
+  {
+    matchs: [
+      {
+        id: "match1",
+        winner: "1",
+        team1: { id: "1", name: "Competitor 1", score: 2 },
+        team2: { id: "2", name: "Competitor 2", score: 1 },
+      },
+      {
+        id: "match2",
+        winner: "4",
+        team1: { id: "3", name: "Competitor 3", score: 0 },
+        team2: { id: "4", name: "Competitor 4", score: 2 },
+      },
+      {
+        id: "match3",
+        winner: "5",
+        team1: { id: "5", name: "Competitor 5", score: 2 },
+        team2: { id: "6", name: "Competitor 6", score: 1 },
+      },
+      {
+        id: "match4",
+        winner: "8",
+        team1: { id: "7", name: "Competitor 7", score: 0 },
+        team2: { id: "8", name: "Competitor 8", score: 2 },
+      },
+    ],
+  },
+  //Semi
+  {
+    matchs: [
+      {
+        id: "match5",
+        winner: "4",
+        team1: { id: "1", name: "Competitor 1", score: 1 },
+        team2: { id: "4", name: "Competitor 4", score: 2 },
+      },
+      {
+        id: "match6",
+        winner: "8",
+        team1: { id: "5", name: "Competitor 5", score: 1 },
+        team2: { id: "8", name: "Competitor 8", score: 2 },
+      },
+    ],
+  },
+  //Final
+  {
+    matchs: [
+      {
+        id: "any_match_id",
+        winner: "8",
+        team1: { id: "4", name: "Competitor 4", score: 1 },
+        team2: { id: "8", name: "Competitor 8", score: 3 },
+      },
+    ],
+  },
+];
 
 const fetchGames = async () => {
   try {
